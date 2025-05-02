9fb6141db8e82a6bcb53b5dffa8905cd8e951be9
Hieu Nguyen <godjungle0908@gmail.com>
2025-05-03 00:15:31 +0700

    delete data

--- src/components/WordMemoryGame/data/wordPairs.ts
+++ src/components/WordMemoryGame/data/wordPairs.ts
@@ -14,18 +14,3 @@ export const wordPairs: WordPair[] = [
   { id: 11, english: "hungry", french: "faim" },
   { id: 12, english: "limestone", french: "calcaire" },
 ];
-
-/*const initialWordPairs: WordPair[] = [
-  { id: 1, english: "forest", french: "forêt" },
-  { id: 2, english: "sibling", french: "frère et sœur" },
-  { id: 3, english: "cereal", french: "céréale" },
-  { id: 4, english: "desk", french: "bureau" },
-  { id: 5, english: "camel", french: "chameau" },
-  { id: 6, english: "butter", french: "beurre" },
-  { id: 7, english: "bicycle", french: "vélo" },
-  { id: 8, english: "railroad", french: "chemin de fer" },
-  { id: 9, english: "folder", french: "dossier" },
-  { id: 10, english: "weekly", french: "hebdomadaire" },
-  { id: 11, english: "hungry", french: "faim" },
-  { id: 12, english: "limestone", french: "calcaire" },
-];*/
